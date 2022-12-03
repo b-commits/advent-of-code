de3074a3e1010f6f183a1d783896e348d206a711
b-commits <bgoscicki@pjwstk.edu.pl>
2022-12-03 11:41:01 +0100

    Day 03: Rucksack Reorganization

--- RucksackReorganization-1/Program.cs
+++ RucksackReorganization-1/Program.cs
@@ -10,25 +10,23 @@ foreach (var rucksack in rucksacks)
 {
     var contents = GetCompartmentContents(rucksack);
     var sharedItem = GetSharedItem(contents.Item1, contents.Item2);
+    var itemTypes = GetItemTypes();
     var priority = GetItemPriority(sharedItem);
     totalPriority += priority;
 }
 
-Console.WriteLine($"Total priority of shared items is {totalPriority}");
+Console.WriteLine($"Total priority of shared items is {totalPriority}.");
 
 (string, string) GetCompartmentContents(string rucksack) 
-    => (rucksack[(rucksack.Length / 2)..], rucksack[..(rucksack.Length / 2)]);
+    => (rucksack[..(rucksack.Length / 2)], rucksack[(rucksack.Length / 2)..]);
 
 
 char GetSharedItem(string firstCompartment, string secondCompartment)
     => firstCompartment.First(secondCompartment.Contains);
 
 IEnumerable<int> GetItemTypes()
-    => Enumerable.Range(ASCII_CAPITAL_A, ALPHABET_COUNT)
-            .Concat(Enumerable.Range(ASCII_LOWERCASE_A, ALPHABET_COUNT)).ToList(); 
+    => Enumerable.Range(ASCII_LOWERCASE_A, ALPHABET_COUNT)
+            .Concat(Enumerable.Range(ASCII_CAPITAL_A, ALPHABET_COUNT)).ToList(); 
 
 int GetItemPriority(char item)
-    => GetItemTypes().ToList().FindIndex(x=> x == item);
-
-
-      
+    => GetItemTypes().ToList().FindIndex(x=> x == item) + 1;
