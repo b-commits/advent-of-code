7601da7e61f9c30ae9378df58524aef06112f550
b-commits <bgoscicki@pjwstk.edu.pl>
2022-12-03 12:28:29 +0100

    Day 03: Rucksack Reorganization

---
+++ RucksackReorganization-02/Program.cs
@@ -0,0 +1,49 @@
+﻿var rucksacks = File.ReadLines("../../../input.txt");
+
+const int ASCII_CAPITAL_A = 65;
+const int ASCII_LOWERCASE_A = 97;
+const int ALPHABET_COUNT = 26;
+
+var totalPriority = 0;
+var threeRucksacks = new List<string>();
+
+foreach (var rucksack in rucksacks)
+{
+    threeRucksacks.Add(rucksack);
+
+    if (threeRucksacks.Count == 3)
+    {
+        var groupBadge = GetGroupBadge(threeRucksacks);
+        var priority = GetItemPriority(groupBadge);
+        totalPriority += priority;
+        threeRucksacks = new List<string>();
+    }
+}
+
+Console.WriteLine($"Total priority of group badges {totalPriority}.");
+
+char? GetGroupBadge(List<string> groupRucksacks)
+{
+    foreach (var groupRucksack in groupRucksacks)
+    {
+        var items = groupRucksack.ToCharArray();
+        
+        foreach (var item in items)
+        {
+            var foundBadge = groupRucksacks.All(rucksack => rucksack.Contains(item));
+            if (foundBadge)
+            {
+                return item;
+            }
+        }
+    }
+
+    return null;
+}
+
+IEnumerable<int> GetItemTypes()
+    => Enumerable.Range(ASCII_LOWERCASE_A, ALPHABET_COUNT)
+        .Concat(Enumerable.Range(ASCII_CAPITAL_A, ALPHABET_COUNT)).ToList(); 
+
+int GetItemPriority(char? item)
+    => GetItemTypes().ToList().FindIndex(x=> x == item) + 1;
