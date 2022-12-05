d850033fa44e3edec633aa3cbaeefc3d77a4e589
b-commits <s19677@pjwstk.edu.pl>
2022-12-05 22:13:16 +0100

    Day 05: Supply Stacks

---
+++ SupplyStacks-01/Program.cs
@@ -0,0 +1,38 @@
+﻿var lines = File.ReadLines("../../../input.txt");
+
+
+var stacks = GetInitialStacks(lines);
+
+foreach (var stack in stacks)
+{
+    stack.RemoveAll(c => c == 32);
+}
+
+
+foreach (var stack in stacks)
+{
+    stack.RemoveAll(c => c == 32);
+}
+
+
+List<List<char>> GetInitialStacks(IEnumerable<string> inputLines)
+{
+    var columns = new List<List<char>>();
+    
+    foreach (var line in inputLines.Select((value, idx) => new { idx, value }))
+    {
+        if (line.value[0] != '[')
+        {
+            return columns;
+        }
+
+        columns.Add(new List<char>());
+        
+        for (var i = 1; i <= 29; i += 4)
+        {
+            columns[line.idx].Add(line.value[i]);
+        }
+    }
+    return columns;
+}
+
